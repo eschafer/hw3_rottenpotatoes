90cf013344138d91cbb4c97d4b0b916bd544f128
Elizabeth Schafer <elizabeth.schafer.wenk@gmail.com>
2013-08-07 03:56:18 +0000

    Happy paths; passing

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -14,7 +14,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  page.body.index(e1) < page.body.index(e2)
 end
 
 Then /I should see all of the movies/ do
