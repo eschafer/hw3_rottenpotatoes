413148f94fcaeb4efbe6431a753724df816505ff
Elizabeth Schafer <elizabeth.schafer.wenk@gmail.com>
2013-08-07 04:18:36 +0000

    Fixed a bug where tests were passing when they shouldn't

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -14,7 +14,9 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  page.body.index(e1) < page.body.index(e2)
+  unless page.body.index(e1) < page.body.index(e2)
+    flunk
+  end
 end
 
 Then /I should see all of the movies/ do
